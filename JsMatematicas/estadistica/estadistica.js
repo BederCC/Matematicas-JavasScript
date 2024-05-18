af3e4ed7fe6f502511ec796fd1623b0aefb92714
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2024-05-18 00:28:38 -0500

    Update estadistica.js

--- JsMatematicas/estadistica/estadistica.js
+++ JsMatematicas/estadistica/estadistica.js
@@ -1,3 +1,29 @@
+function esPar(lista){
+    return !(lista.length % 2);
+}
+function esImpar(lista){
+    return lista.length % 2;
+}
+
+function calcularMediana(listaDesordenada){
+    const lista = ordenarLista(listaDesordenada);
+    const listaEsPar = esPar(lista);
+
+    if(listaEsPar){
+        const mitadLista1 = lista[lista.length / 2];
+        const mitadLista2 = lista[(lista.length / 2) - 1];
+        const listaMitades = [mitadLista1, mitadLista2];
+        const medianaListaPar = calcularPromedio(listaMitades);
+        return medianaListaPar;
+    }else{
+        const indexMitadListaImpar = Math.ceil(lista.length / 2);
+        const medianaListaImpar = lista[indexMitadListaImpar -1];
+        console.log(indexMitadListaImpar);
+        console.log(medianaListaImpar);
+        return lista[indexMitadListaImpar - 1];
+    }    
+}    
+
 function calcularPromedio(lista){
     // let sumaLista = 0;
     // for(let i = 0; i < lista.length; i++){
@@ -12,7 +38,17 @@ function calcularPromedio(lista){
 
 
     const promedio = sumaLista / lista.length;
-    console.log(promedio);
+    //console.log(promedio);
     return promedio;
     
 }
+
+function ordenarLista(listaDesordenada){
+function ordenarListaSort(valorAcumulado, nuevoValor){
+    return valorAcumulado - nuevoValor;
+}
+
+    const lista = listaDesordenada.sort(ordenarListaSort);
+    return lista;
+}
+    
